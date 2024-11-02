b99782f489f34e93f7780d7d8bc36bedd3d7d026
ahujaankush <aahuja06@proton.me>
2024-11-03 00:35:30 +0100

    WorttrainerFrame Implementation

--- src/main/java/aahuja/tgm/ac/at/App.java
+++ src/main/java/aahuja/tgm/ac/at/App.java
@@ -1,10 +1,14 @@
 package aahuja.tgm.ac.at;
 
+import aahuja.tgm.ac.at.view.WorttrainerFrame;
+
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+  public static void main(String[] args) {
+    System.setProperty("awt.useSystemAAFontSettings", "on");
+    System.setProperty("swing.aatext", "true");
+    new WorttrainerFrame();
+  }
 }
